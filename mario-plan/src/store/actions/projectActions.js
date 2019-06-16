80dda5796e2aaea6d0b832180204f158e455762a
erscriber <erscriber@gmail.com>
2019-06-16 15:50:40 -0400

    connected react redux and firebase

--- mario-plan/src/store/actions/projectActions.js
+++ mario-plan/src/store/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFiresstore }) => {
         //make async call to database
         dispatch({ type: 'CREATE_PROJECT', project });
     }
